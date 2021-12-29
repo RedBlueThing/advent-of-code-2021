56eba3d0cfb4674c028319feb5c8ef343e82dabb
Tom Horn <tom.horn@gmail.com>
2021-12-29 17:05:20 +1100

    Day twenty three. First A* implementation in a couple of decades.

--- day-twenty-three.py
+++ day-twenty-three.py
@@ -1,11 +1,10 @@
 import functools
 import math
 from enum import Enum
-import numpy as np
 import random
 import copy
 from itertools import groupby
-
+import heapq
 
 def flatten(items):
     """Yield items from any nested iterable; see Reference."""
@@ -19,6 +18,8 @@ def flatten(items):
 real_data = ["#############\n", "#...........#\n", "###D#D#B#A###\n", "  #C#A#B#C#\n", "  #########\n"]
 test_data = ["#############\n", "#...........#\n", "###B#C#B#D###\n", "  #A#D#C#A#\n", "  #########\n"]
 part_two_insert = ["  #D#C#B#A#\n", "  #D#B#A#C#\n"]
+part_two_real_data = real_data[:3] + part_two_insert + real_data[3:]
+part_two_test_data = test_data[:3] + part_two_insert + test_data[3:]
 
 energy_cost = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
 
@@ -40,10 +41,11 @@ def cost(steps, amphipod):
 #########
 
 # 16404 -- Too low (no constraints cost)
-# cost(7,'D') + cost(8, 'D') + cost(9, 'A') + cost(5, 'A') + cost(5, 'B') + cost(4, 'B') +  cost(6,'C') + cost(7, 'C')
-
-test_cost = cost(7, 'D') + cost(8, 'D') + cost(3, 'A') + cost(7, 'A') + cost(5, 'B') + cost(5, 'B') + cost(
-    7, 'B') + cost(7, 'B') + cost(6, 'C') + cost(7, 'C') + cost(9, 'A') + cost(9, 'A')
+part_one_real_no_constraints_cost = cost(7, 'D') + cost(8, 'D') + cost(9, 'A') + cost(5, 'A') + cost(5, 'B') + cost(
+    4, 'B') + cost(6, 'C') + cost(7, 'C')
+#
+# The no constraints cost could be the "admissible" (never overestimates the cost) heursitic for A* search!!!! So
+# I need a function to calculate this automagically!
 
 # calculated ->
 # {'cost': 16558, 'side_room_rear': ['A', 'B', 'C', 'D'], 'side_room_entrance': ['A', 'B', 'C', 'D'], 'hall': [None, None, None, None, None, None, None, None, None, None, None]}
@@ -62,25 +64,45 @@ def read_game_state(lines):
     def none_or_amphipod(characters):
         return [None if x == "." else x for x in characters]
 
-    side_room_entrance = lines[2][3:].split("#")[:4]
-    side_room_rear = (lines[3]).strip()[1:].split("#")[:4]
     hall = lines[1].strip()[1:-1]
-    return {
-        "cost": 0,
-        "side_room_rear": none_or_amphipod(side_room_rear),
-        "side_room_entrance": none_or_amphipod(side_room_entrance),
-        "hall": none_or_amphipod(hall)
-    }
+    side_rooms = []
+    for line in lines[2:]:
+        split_line = line.strip().strip("#").split("#")
+        if (len(split_line) > 1):
+            side_rooms.append(none_or_amphipod(split_line))
+
+    return {"cost": 0, "side_rooms": side_rooms, "hall": none_or_amphipod(hall)}
+
+
+def key_for_game_state(game_state):
+    return "".join([x or "." for x in list(flatten(game_state.get('side_rooms'))) + game_state.get("hall")])
+
+
+y = key_for_game_state(read_game_state(test_data))
 
 
 def amphipod_for_descriptor(game_state, descriptor):
     key, index = descriptor.split(":")
+    if ("side_rooms" in descriptor):
+        key, depth_index = key.split("-")
+        try:
+            return game_state[key][int(depth_index)][int(index)]
+        except:
+            print(game_state)
+            print(descriptor)
+            raise
+
     return game_state[key][int(index)]
 
 
 def update_descriptor(game_state, descriptor, new_value):
     key, index = descriptor.split(":")
-    game_state[key][int(index)] = new_value
+
+    if ("side_rooms" in descriptor):
+        key, depth_index = key.split("-")
+        game_state[key][int(depth_index)][int(index)] = new_value
+    else:
+        game_state[key][int(index)] = new_value
 
 
 def descriptor_for_key_and_index(key, index):
@@ -96,7 +118,14 @@ def hall_descriptor(descriptor):
 
 
 def entrance_descriptor(descriptor):
-    return "side_room_entrance" in descriptor
+    return "side_rooms-0" in descriptor
+
+
+def steps_to_side_room_descriptor(descriptor):
+    assert "side_rooms" in descriptor
+    key, index = descriptor.split(":")
+    key, depth_index = key.split("-")
+    return int(depth_index) + 1
 
 
 def target_room_index_for_amphipod(amphipod):
@@ -118,13 +147,20 @@ def target_room_index_for_position(game_state, descriptor):
     return target_room_index_for_amphipod(amphipod_for_descriptor(game_state, descriptor))
 
 
+def side_rooms_for_index(game_state, target_room_index):
+    return [x[target_room_index] for x in game_state.get('side_rooms')]
+
+
 def available_target_room(game_state, descriptor, target_room_index):
-    if (game_state.get("side_room_entrance")[target_room_index] is not None):
+
+    free_spots = [x is None for x in side_rooms_for_index(game_state, target_room_index)]
+    if all(free_spots):
+        return True
+    if not any(free_spots):
         return False
-    if (game_state.get("side_room_rear")[target_room_index] is not None
-            and game_state.get("side_room_rear")[target_room_index] != amphipod_for_descriptor(game_state, descriptor)):
-        return False
-    return True
+
+    our_amphipod = amphipod_for_descriptor(game_state, descriptor)
+    return all([x == our_amphipod for x in side_rooms_for_index(game_state, target_room_index) if x is not None])
 
 
 def available_to_move(game_state):
@@ -132,17 +168,15 @@ def available_to_move(game_state):
     Return a list of amphipods that can move
     """
     room_candidates = []
-    for index, side_room_amphipod in enumerate(game_state.get("side_room_entrance")):
 
-        # If there is a amphipod in the entrance room (and they don't belong
-        # there already or the rear room doesn't belong there either)
-        if side_room_amphipod is not None and (
-                target_room_index_for_amphipod(side_room_amphipod) != index
-                or target_room_index_for_amphipod(game_state.get("side_room_rear")[index]) != index):
-            room_candidates.append(descriptor_for_key_and_index("side_room_entrance", index))
-        elif game_state.get("side_room_rear")[index] is not None and target_room_index_for_amphipod(
-                game_state.get("side_room_rear")[index]) != index:
-            room_candidates.append(descriptor_for_key_and_index("side_room_rear", index))
+    # for each of the side rooms (corridors)
+    for index in range(0, len(game_state.get("side_rooms")[0])):
+
+        # If any one in this list doesn't belong here, then we need to move the top one
+        side_rooms_for_this_index = side_rooms_for_index(game_state, index)
+        if (any([target_room_index_for_amphipod(x) != index for x in side_rooms_for_this_index if x is not None])):
+            first_ampipod_index = len([x for x in side_rooms_for_this_index if x is None])
+            room_candidates.append(descriptor_for_key_and_index("side_rooms-%d" % first_ampipod_index, index))
 
     # Check the hall guys
     hall_candidates = []
@@ -159,17 +193,17 @@ def available_to_move(game_state):
 
 
 assert available_to_move(read_game_state(test_data)) == {
-    "room_candidates": ['side_room_entrance:0', 'side_room_entrance:1', 'side_room_entrance:2', 'side_room_entrance:3'],
+    "room_candidates": ['side_rooms-0:0', 'side_rooms-0:1', 'side_rooms-0:2', 'side_rooms-0:3'],
     "hall_candidates": []
 }
 assert available_to_move(
     read_game_state(["#############\n", "#..........A#\n", "###.#C#B#D###\n", "  #A#D#C#A#\n", "  #########\n"])) == {
-        "room_candidates": ['side_room_entrance:1', 'side_room_entrance:2', 'side_room_entrance:3'],
+        "room_candidates": ['side_rooms-0:1', 'side_rooms-0:2', 'side_rooms-0:3'],
         "hall_candidates": ['hall:10']
     }
 assert available_to_move(
     read_game_state(["#############\n", "#..........A#\n", "###.#C#B#D###\n", "  #D#A#C#A#\n", "  #########\n"])) == {
-        "room_candidates": ['side_room_rear:0', 'side_room_entrance:1', 'side_room_entrance:2', 'side_room_entrance:3'],
+        "room_candidates": ['side_rooms-1:0', 'side_rooms-0:1', 'side_rooms-0:2', 'side_rooms-0:3'],
         "hall_candidates": []
     }
 
@@ -233,10 +267,9 @@ def available_destinations(game_state):
         ]
 
     def destination_for_target_room_index(game_state, target_room_index):
-        return [descriptor_for_key_and_index("side_room_entrance", target_room_index)
-                ] if game_state.get("side_room_rear")[target_room_index] else [
-                    descriptor_for_key_and_index("side_room_rear", target_room_index)
-                ]
+        side_rooms_for_this_index = side_rooms_for_index(game_state, target_room_index)
+        free_depth_index = len([x for x in side_rooms_for_this_index if x is None]) - 1
+        return [descriptor_for_key_and_index("side_rooms-%d" % free_depth_index, target_room_index)]
 
     # Then overwrite those side room candidates if they can move directly into
     # their target room (because that will always be cheaper than a move to the
@@ -277,16 +310,16 @@ def available_destinations(game_state):
 
 assert available_destinations(
     read_game_state(["#############\n", "#.A.........#\n", "###.#C#A#B###\n", "  #A#D#C#A#\n",
-                     "  #########\n"]))['side_room_entrance:1'] == ['hall:3', 'hall:5', 'hall:7', 'hall:9', 'hall:10']
+                     "  #########\n"]))['side_rooms-0:1'] == ['hall:3', 'hall:5', 'hall:7', 'hall:9', 'hall:10']
 assert available_destinations(
     read_game_state(["#############\n", "#.A.........#\n", "###.#C#.#B###\n", "  #A#D#.#A#\n",
-                     "  #########\n"]))['side_room_entrance:1'] == ['side_room_rear:2']
+                     "  #########\n"]))['side_rooms-0:1'] == ['side_rooms-1:2']
 assert available_destinations(
     read_game_state(["#############\n", "#.A...D.....#\n", "###.#C#.#B###\n", "  #A#D#.#A#\n",
-                     "  #########\n"]))['side_room_entrance:1'] == ['hall:3']
+                     "  #########\n"]))['side_rooms-0:1'] == ['hall:3']
 assert available_destinations(
     read_game_state(["#############\n", "#C....D....C#\n", "###A#B#.#.###\n", "  #A#B#.#D#\n",
-                     "  #########\n"]))['hall:5'] == ['side_room_entrance:3']
+                     "  #########\n"]))['hall:5'] == ['side_rooms-0:3']
 
 
 def cost_for_move(game_state, source, target):
@@ -294,24 +327,25 @@ def cost_for_move(game_state, source, target):
     # first, which amphipod is moving?
     steps = 0
     amphipod = amphipod_for_descriptor(game_state, source)
-    assert amphipod_for_descriptor(game_state, target) == None
+
+    # print("%s -> %s" % (source, target))
 
     # If they are moving from the hall?
     if (hall_descriptor(source)):
         target_hall_index = hall_index_for_room_index(index_for_descriptor(target))
         steps += abs(target_hall_index - index_for_descriptor(source))
-        steps += 1 if entrance_descriptor(target) else 2
+        steps += steps_to_side_room_descriptor(target)
     elif hall_descriptor(target):
         # Just move into the side room
-        steps += 1 if entrance_descriptor(source) else 2
+        steps += steps_to_side_room_descriptor(source)
         # move from the side room to the spot in the hall
         target_hall_index = index_for_descriptor(target)
         source_hall_index = hall_index_for_room_index(index_for_descriptor(source))
         steps += abs(target_hall_index - source_hall_index)
     else:
         # move from and to the side room
-        steps += 1 if entrance_descriptor(source) else 2
-        steps += 1 if entrance_descriptor(target) else 2
+        steps += steps_to_side_room_descriptor(source)
+        steps += steps_to_side_room_descriptor(target)
         # move from the side room to the other side room via the hall
         target_hall_index = hall_index_for_room_index(index_for_descriptor(target))
         source_hall_index = hall_index_for_room_index(index_for_descriptor(source))
@@ -342,56 +376,33 @@ def filter_for_none(spots):
     return [spot for spot in spots if spot is not None]
 
 
-def verify_game_state(game_state):
-    assert "".join(
-        sorted(
-            filter_for_none(
-                game_state.get("hall") + game_state.get("side_room_entrance") +
-                game_state.get("side_room_rear")))) == "AABBCCDD"
-
-
 def finished_game_state(game_state):
 
     try:
-        verify_game_state(game_state)
-        return "".join(filter_for_none(game_state.get('side_room_rear'))) == "ABCD" and "".join(
-            filter_for_none(game_state.get('side_room_entrance'))) == "ABCD"
+        return all(["".join(filter_for_none(x)) == "ABCD" for x in game_state.get("side_rooms")])
     except:
         print(game_state)
         raise
 
 
 assert finished_game_state({
-    'cost': 0,
-    'side_room_rear': ['A', 'D', 'C', 'A'],
-    'side_room_entrance': ['B', 'C', 'B', 'D'],
+    'cost':
+    0,
+    'side_rooms': [['B', 'C', 'B', 'D'], ['D', 'C', 'B', 'A'], ['D', 'B', 'A', 'C'], ['A', 'D', 'C', 'A']],
     'hall': [None, None, None, None, None, None, None, None, None, None, None]
 }) == False
 assert finished_game_state({
-    'cost': 999,
-    'side_room_rear': ['A', 'B', 'C', 'D'],
-    'side_room_entrance': ['A', 'B', 'C', 'D'],
+    'cost':
+    999,
+    'side_rooms': [['A', 'B', 'C', 'D'], ['A', 'B', 'C', 'D'], ['A', 'B', 'C', 'D'], ['A', 'B', 'C', 'D']],
     'hall': [None, None, None, None, None, None, None, None, None, None, None]
 }) == True
 
-min_cost = 50000
-min_state = {}
-
 
 def brute_force(game_state):
-    global min_cost
-    global min_state
 
     if (finished_game_state(game_state)):
-        if (game_state.get('cost') < min_cost):
-            min_cost = game_state.get('cost')
-            min_state = game_state
-            print(min_state)
         return game_state.get('cost')
-
-    # Abandon this path
-    if (game_state.get('cost') > 20000):
-        return None
 
     try:
         all_moves = flatten_and_cost(game_state, available_destinations(game_state))
@@ -409,5 +420,91 @@ def brute_force(game_state):
     ]
 
 
-real_game_state = read_game_state(real_data)
-results = brute_force(real_game_state)
+def heuristic_solution_cost(game_state):
+    """
+    This function uses the "no constraints cost" to get to the completed game
+    state for this game state. Because this will always be less than the actual
+    cost, we can use this for the A* search heuristic.
+    """
+    heuristic_cost = 0
+    # Just move to the same row in the right room index. So The cost is less
+    # than the "no constraints" cost I created manually.
+    for row_index, side_room_row in enumerate(game_state.get("side_rooms")):
+        for index, amphipod in enumerate(side_room_row):
+            if (amphipod is None):
+                continue
+            target_room_index = target_room_index_for_amphipod(amphipod)
+            if target_room_index != index:
+                heuristic_cost += cost_for_move(
+                    game_state, descriptor_for_key_and_index("side_rooms-%d" % row_index, index),
+                    descriptor_for_key_and_index("side_rooms-%d" % row_index, target_room_index))
+
+    # Don't want to keep track of the free spots in the side rooms, so our
+    # heuristic will just calculate the move from the hall to the first room
+    # spot. Stays admissible because that cost will always be less than or
+    # equal the actual cost to move these guys from the hall to their target
+    # rooms.
+    for hall_index, amphipod in enumerate(game_state.get('hall')):
+        if (amphipod is None):
+            continue
+        target_room_index = target_room_index_for_amphipod(amphipod)
+        heuristic_cost += cost_for_move(game_state, descriptor_for_key_and_index("hall", hall_index),
+                                        descriptor_for_key_and_index("side_rooms-0", target_room_index))
+
+    return heuristic_cost
+
+
+class PriorityElem:
+    def __init__(self, elem_to_wrap, priority):
+        self.wrapped_elem = elem_to_wrap
+        self.priority = priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
+def a_star(game_state):
+    def push_game_state(open_set, game_state):
+        try:
+            heapq.heappush(open_set,
+                           PriorityElem(game_state,
+                                        game_state.get("cost") + heuristic_solution_cost(game_state)))
+        except:
+            print(game_state)
+            raise
+
+    def pop_game_state(open_set):
+        return heapq.heappop(open_set).wrapped_elem
+
+    open_set = []
+
+    # start with the initial node
+    push_game_state(open_set, game_state)
+
+    g_scores = {key_for_game_state(game_state): 0}
+
+    while (open_set):
+
+        current = pop_game_state(open_set)
+
+        if (finished_game_state(current)):
+            return current
+
+        all_moves = flatten_and_cost(current, available_destinations(current))
+
+        for move in all_moves:
+            neighbour_game_state = apply_move(current, move)
+
+            current_g_score = g_scores.get(key_for_game_state(neighbour_game_state))
+            if current_g_score is None or neighbour_game_state.get("cost") < current_g_score:
+                g_scores[key_for_game_state(neighbour_game_state)] = neighbour_game_state.get("cost")
+                push_game_state(open_set, neighbour_game_state)
+
+
+# part two
+# test_game_state = read_game_state(part_two_test_data)
+# results = a_star(test_game_state)
+# test_game_state = read_game_state(test_data)
+# results = a_star(test_game_state)
+real_game_state = read_game_state(part_two_real_data)
+results = a_star(real_game_state)
